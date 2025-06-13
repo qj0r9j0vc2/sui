513ca2ce7760d6a991307efc0bdeb9f51a72b2ab
Jinu <me@qj0r9j0vc2.me>
2025-06-13 18:56:35 +0900

    Add testnet DAG release tag

--- RELEASES.md
+++ RELEASES.md
@@ -38,3 +38,5 @@ https://github.com/MystenLabs/sui/releases
 * 22-05-24: [0.3.0](https://github.com/MystenLabs/sui/releases/tag/devnet-0.3.0-rc)
 * 22-05-12: [0.2.0](https://medium.com/mysten-labs/sui-release-notes-v0-2-0-7b377e2bf01)
 * 22-05-05: [0.1.0 (Sui Devnet launch)](https://medium.com/mysten-labs/sui-devnet-public-release-a2be304ff36b)
+
+* 25-06-13: [1.50.1-dag](https://github.com/qj0r9j0vc2/sui/releases/tag/testnet-v1.50.1-dag)
---
+++ crates/sui-json-rpc/src/dag_api.rs
@@ -0,0 +1,53 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use jsonrpsee::core::RpcResult;
+use jsonrpsee::RpcModule;
+use parking_lot::RwLock;
+use consensus_core::DagState;
+use sui_open_rpc::Module;
+use sui_json_rpc_api::{DagReadApiOpenRpc, DagReadApiServer, JsonRpcMetrics};
+use sui_json_rpc_types::SuiDagBlock;
+
+use crate::{with_tracing, SuiRpcModule, error::Error};
+
+#[derive(Clone)]
+pub struct DagReadApi {
+    dag_state: Arc<RwLock<DagState>>, 
+    _metrics: Arc<JsonRpcMetrics>,
+}
+
+impl DagReadApi {
+    pub fn new(dag_state: Arc<RwLock<DagState>>, metrics: Arc<JsonRpcMetrics>) -> Self {
+        Self { dag_state, _metrics: metrics }
+    }
+}
+
+#[async_trait]
+impl DagReadApiServer for DagReadApi {
+    async fn get_latest_dag_blocks(&self, num_rounds: Option<u64>) -> RpcResult<Vec<SuiDagBlock>> {
+        with_tracing!(async move {
+            let num_rounds = num_rounds.unwrap_or(5);
+            let ds = self.dag_state.read();
+            let highest = ds.highest_accepted_round();
+            let start = highest.saturating_sub(num_rounds as u32);
+            let mut blocks = Vec::new();
+            for i in 0..ds.committee().size() {
+                let author = ds.committee().to_authority_index(i).unwrap();
+                let mut b = ds.get_cached_blocks_in_range(author, start, highest + 1, usize::MAX);
+                blocks.append(&mut b);
+            }
+            Ok(blocks.into_iter().map(SuiDagBlock::from).collect())
+        })
+    }
+}
+
+impl SuiRpcModule for DagReadApi {
+    fn rpc(self) -> RpcModule<Self> {
+        self.into_rpc()
+    }
+
+    fn rpc_doc_module() -> Module {
+        DagReadApiOpenRpc::module_doc()
+    }
+}
